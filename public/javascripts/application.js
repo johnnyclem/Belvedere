1455da7424557832ac3b4825c480e419a0322537
John Clem <johnnyclem@gmail.com>
2011-05-31 13:51:41 -0700

    fixed stupid deprication warning

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,19 +8,4 @@ return false;
 	return false;
 }	
 });
-$( "#dialog:ui-dialog" ).dialog( "destroy" );
-	
-		$( "#dialog-confirm" ).dialog({
-			resizable: false,
-			height:140,
-			modal: true,
-			buttons: {
-				"Delete all items": function() {
-					$( this ).dialog( "close" );
-				},
-				Cancel: function() {
-					$( this ).dialog( "close" );
-				}
-			}
-		});	
 });
